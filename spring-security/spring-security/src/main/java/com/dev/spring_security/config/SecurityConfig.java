3297457f2897698e76b7dcea73708251d08a8654
sanghak lee <yee950419@naver.com>
2025-03-12 14:27:05 +0900

    1. 권한이 없는 접근 요청 시 자동으로 login 화면으로 redirect 시키는 로직 추가
    2. 원활한 로그인 테스트를 위해 csrf 비활성화 (임시)

--- spring-security/spring-security/src/main/java/com/dev/spring_security/config/SecurityConfig.java
+++ spring-security/spring-security/src/main/java/com/dev/spring_security/config/SecurityConfig.java
@@ -11,12 +11,21 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 
+        // 접근 경로 제어 로직
         http
                 .authorizeHttpRequests((auth) -> auth
                         .requestMatchers("/", "/login").permitAll()             // 해당 경로에 대한 접근은 모두 허용
                         .requestMatchers("/admin").hasRole("ADMIN")                // 특정 role이 있는 사용자는 접근 허용
                         .requestMatchers("/my/**").hasAnyRole("ADMIN", "USER")           // /my/로 지삭되는 경로에 대해서, 여러 개의 role 중 해닫되는 것이 있는 사용자는 접근 허용
                         .anyRequest().authenticated());                             // 나머지 접근은 인증된 사용자만 접근 허용
+
+        http
+                .formLogin((auth) -> auth.loginPage("/login").permitAll()   // 권한이 없는 page로 접근하였을 때, 자동으로 /login 경로로 redirect 시켜주는 설정
+                        .loginProcessingUrl("/loginProc")
+                        .permitAll());
+
+        http
+                .csrf((auth) -> auth.disable());    // 사이트 위변조방지설정 동작 시, post 요청은 csrf 토큰도 같이 보내줘야 로그인이 동작하므로, 임시적으로 비활성화
 
         return http.build();
     }
