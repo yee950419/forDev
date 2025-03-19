d87ff48608e49fd100f288ea3f21ad0fe853b0ce
sanghak lee <yee950419@naver.com>
2025-03-19 14:35:59 +0900

    1. 세션 현재 사용자의 아이디와 role 반환 추가

--- spring-security-jwt/spring-security-jwt/src/main/java/com/dev/spring_security_jwt/controller/MainController.java
+++ spring-security-jwt/spring-security-jwt/src/main/java/com/dev/spring_security_jwt/controller/MainController.java
@@ -1,7 +1,13 @@
 package com.dev.spring_security_jwt.controller;
 
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Collection;
+import java.util.Iterator;
 
 @RestController
 public class MainController {
@@ -9,7 +15,18 @@ public class MainController {
     @GetMapping("/")
     public String mainPage() {
 
-        return "Main Controller";
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+
+        // 세션 현재 사용자 아이디
+        String username = authentication.getName();
+
+        // 세션 현재 사용자 role
+        Collection<? extends GrantedAuthority> authorities = authentication.getAuthorities();
+        Iterator<? extends GrantedAuthority> iter = authorities.iterator();
+        GrantedAuthority auth = iter.next();
+        String role = auth.getAuthority();
+
+        return "Main Controller " + username + " " + role;
     }
 
 }
