63d767c5dc877e58310fa5bd948e5d3d918caa6b
sanghak lee <yee950419@naver.com>
2025-03-24 17:48:23 +0900

    1. Job 실행 api 추가 2. Job 실행 용 Scheduler 추가

---
+++ springBatch/springBatch/src/main/java/com/template/springBatch/controller/MainController.java
@@ -0,0 +1,31 @@
+package com.template.springBatch.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.batch.core.JobParameters;
+import org.springframework.batch.core.JobParametersBuilder;
+import org.springframework.batch.core.configuration.JobRegistry;
+import org.springframework.batch.core.launch.JobLauncher;
+import org.springframework.batch.core.repository.JobRepository;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequiredArgsConstructor
+@RestController
+public class MainController {
+
+    private final JobLauncher jobLauncher;
+    private final JobRegistry jobRegistry;
+
+    @GetMapping("/first")
+    public String firstApi(@RequestParam String value) throws Exception {
+
+        JobParameters jobParameters = new JobParametersBuilder()
+                .addString("date", value)
+                .toJobParameters();
+
+        jobLauncher.run(jobRegistry.getJob("firstJob"), jobParameters);
+
+        return "ok";
+    }
+}
